2f6b3d64102ca88bbb6df4881a4abc0de4b140ae
rayane moara <rayane_moara@hotmail.com>
2025-01-31 14:38:34 -0300

    Subindo crud temas

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,62 @@
+import { ReactNode, createContext, useState } from "react";
+import UsuarioLogin from "../models/UsuarioLogin";
+import { login } from "../services/Service";
+
+interface AuthContextProps {
+    usuario: UsuarioLogin
+    handleLogout(): void
+    handleLogin(usuario: UsuarioLogin): Promise<void>
+    isLoading: boolean
+}
+
+interface AuthProvidersProps {
+    children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextProps)
+
+export function AuthProvider({ children }: AuthProvidersProps) {
+
+    const [usuario, setUsuario] = useState<UsuarioLogin>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        senha: '',
+        foto: '',
+        token: ''
+    })
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    async function handleLogin(usuarioLogin: UsuarioLogin) {
+        setIsLoading(true)
+
+        try {
+            await login(`/usuarios/logar`, usuarioLogin, setUsuario)
+            alert("O Usuário foi autenticado com sucesso!")
+        } catch (error) {
+            console.log(error)
+            alert("Os dados do Usuário estão inconsistentes!")
+        }
+
+        setIsLoading(false)
+        
+    }
+
+    function handleLogout() {
+        setUsuario({
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        })
+    }
+
+    return(
+        <AuthContext.Provider value={{ usuario, handleLogin, handleLogout, isLoading}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
---
+++ src/models/Usuario.ts
@@ -0,0 +1,9 @@
+import Postagem from "./Postagem";
+export default interface Usuario {
+  id: number;
+  nome: string;
+  usuario: string;
+  foto: string;
+  senha: string;
+  postagem?: Postagem | null;
+}
